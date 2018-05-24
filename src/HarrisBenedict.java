d2ac2ebf7a7e8b93a404e7d639e44c56f051eb33
jjh <jjhn92@ajou.ac.kr>
2018-05-24 21:13:52 +0900

    일 목표소모 열량 운동 식사 계획 비율 구현

--- src/HarrisBenedict.java
+++ src/HarrisBenedict.java
@@ -1,38 +1,59 @@
+import java.util.Arrays;
+import java.util.List;
 
 public class HarrisBenedict {
-	public double bmiCal(float height,double weight){
-		return weight/((height/100)*(height/100));
+	public int bmiCal(float height,float weight){
+		return (int)(weight/((height/100)*(height/100)));
 	}
-	public static float bmrCal(float height,double weight,int age,String sex) { //기초 대사량 계산
+	public static float bmrCal(float height,float weight,int age,String sex) { //기초 대사량 계산
+		float bmrcal = 0;
 		if(sex =="m") {
-			return (float) (88.362+(13.397*weight)+(4.799*height)-(5.677*age));
+			bmrcal = (int) (88.362+(13.397*weight)+(4.799*height)-(5.677*age));
 		}
 		else if(sex =="w") {
-			return (float) (447.593+(9.247*weight)+(3.098*height)-(4.33*age));
+			bmrcal = (int) (447.593+(9.247*weight)+(3.098*height)-(4.33*age));
 		}
-		return 0;
+		return bmrcal;
 	}
 	
-	public static float actCal(float height,double weight,int age,String sex,int choice) { //활동대사량 계산
+	public static float actCal(float height,float weight,int age,String sex,int choice) { //활동대사량 계산
+		float actcal = 0;
 		switch(choice){
 	        case 1: //활동이 적거나 운동을 안할 경우
-	        	return (float) (bmrCal(height, weight, age, sex)*0.2);
+	        	actcal = (float) (bmrCal(height, weight, age, sex)*0.2);
 			case 2: //가벼운 활동 및 운동(대략 1-3일/주)
-	        	return (float) (bmrCal(height, weight, age, sex)*0.375);
+	        	actcal = (float) (bmrCal(height, weight, age, sex)*0.375);
 	        case 3: // 보통의 활동 및 운동(대략 3-5일/주)
-	        	return (float) (bmrCal(height, weight, age, sex)*0.555);
+	        	actcal =  (float) (bmrCal(height, weight, age, sex)*0.555);
 	        case 4: //적극적인 활동 및 운동(대략 6-7일/주)
-	        	return (float) (bmrCal(height, weight, age, sex)*0.725);
+	        	actcal = (float) (bmrCal(height, weight, age, sex)*0.725);
 	        case 5: //아주 적극적인 활동 및 운동
-	        	return (float) (bmrCal(height, weight, age, sex)*0.9);	
+	        	actcal = (float) (bmrCal(height, weight, age, sex)*0.9);	
 	        default:    
 		}
-		return 0;
+		return actcal;
 	}
-	public static float tefCal(float height,double weight,int age,String sex,int choice) {
+	public static float tefCal(float height,float weight,int age,String sex,int choice) { //음식소화 흡수열량 계산
 		return (actCal(height,weight,age,sex,choice)+bmrCal(height,weight,age,sex))/9;
 	}
-	public static float consumeCal(float height,double weight,int age,String sex,int choice) {
+	public static float consumeCal(float height,float weight,int age,String sex,int choice) { //일일 소비칼로리 계산
 		return actCal(height,weight,age,sex,choice)+bmrCal(height,weight,age,sex)+tefCal(height,weight,age,sex,choice);
+	}
+	public static float targetCal(float weight, float targetWeight,int targetDay) { //일 목표소비 칼로리 계산
+		return ((weight - targetWeight) * 7200)/targetDay;
+	}
+	/*public static void exerCal(float weight, float targetWeight,int targetDay) {
+		targetCal(weight,targetWeight,targetDay)*0.3;
+	}
+	public static void foodCal(float weight, float targetWeight,int targetDay) {
+		targetCal(weight,targetWeight,targetDay)*0.7;
+	}*/
+	public static List<Float> ratioCal(float weight, float targetWeight,int targetDay){
+		return Arrays.asList((float)(targetCal(weight,targetWeight,targetDay)*0.3),(float)(targetCal(weight,targetWeight,targetDay)*0.7));
+	}
+	public static void usingRCal(float weight,float targetWeight,int targetDay) {
+		List<Float> result = ratioCal(weight, targetWeight, targetDay);
+		System.out.println("\n운동으로 소모해야할 칼로리:"+result.get(0)+"kcal");
+		System.out.println("식사조절로 줄여야할 칼로리:"+result.get(1)+"kcal");
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,30 @@
+import java.util.List;
 
 public class Main {
 	private float height;
-	private double weight;
+	private float weight;
 	private int age;
 	private String sex;
 	private int choice;
-	public Main(float height,double weight,int age,String sex,int choice) {
+	private float targetWeight;
+	private int targetDay;
+	public Main(float height,float weight,float targetWeight,int age,String sex,int choice,int targetDay) {
 		this.height = height;
 		this.weight = weight;
+		this.targetWeight = targetWeight;
 		this.age = age;
 		this.sex = sex;
 		this.choice = choice;
+		this.targetDay = targetDay;
 	}
 	public float getHeight() {
 		return height;
 	}
-	public double getWeight() {
+	public float getWeight() {
 		return weight;
+	}
+	public float getTWeight() {
+		return targetWeight;
 	}
 	public int getAge() {
 		return age;
@@ -27,14 +35,20 @@ public class Main {
 	public int getChoice() {
 		return choice;
 	}
+	public int getTDay() {
+		return targetDay;
+	}
+	
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
-		Main m = new Main(181,81,27,"m",4);
+		Main m = new Main(181,82,79,27,"m",4,90); // 키, 체중, 목표체중, 나이, 성별, 일활동량, 목표기간
 		HarrisBenedict hb = new HarrisBenedict();
-		System.out.printf("BMI: %.2fkcal\n",hb.bmiCal(m.getHeight(),m.getWeight()));
+		System.out.printf("BMI: %d\n",hb.bmiCal(m.getHeight(),m.getWeight()));
 		System.out.printf("기초대사량(BMR): %.2fkcal\n",hb.bmrCal(m.getHeight(),m.getWeight(),m.getAge(),m.getSex()));
 		System.out.printf("활동대사량: %.2fkcal\n",hb.actCal(m.getHeight(), m.getWeight(), m.getAge(), m.getSex(), m.getChoice()));
 		System.out.printf("음식소화흡수열량: %.2fkcal\n",hb.tefCal(m.getHeight(), m.getWeight(), m.getAge(), m.getSex(), m.getChoice()));
-		System.out.printf("일일 소비 칼로리: %.2fkcal",hb.consumeCal(m.getHeight(), m.getWeight(), m.getAge(), m.getSex(), m.getChoice()));
+		System.out.printf("일일 소비 칼로리: %.2fkcal\n",hb.consumeCal(m.getHeight(), m.getWeight(), m.getAge(), m.getSex(), m.getChoice()));
+		System.out.printf("일 목표 소모 칼로리: %.2fkcal",hb.targetCal(m.getWeight(),m.getTWeight(),m.getTDay()));
+		hb.usingRCal(m.getWeight(), m.getTWeight(), m.getTDay());
 	}
 }
