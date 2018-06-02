5e1f7bc3dea9d6140febf0f946aa2cfd60cab116
jungwoo <okjwoo@ajou.ac.kr>
2018-06-03 04:35:23 +0900

    daily입력 수정

--- routes/daily-router.js
+++ routes/daily-router.js
@@ -3,8 +3,18 @@ var router = express.Router();
 var dailyAdapter = require('../adapters/appdaily-db-adapter');
 
 router.post('/', function(req, res) {
-    dailyAdapter.dailyWrite(function(err,rows){
-        res.json(rows);
+
+    console.log(req.body);
+    var str = Object.keys(req.body);
+    var obj = JSON.parse(str[0]);
+
+    dailyAdapter.dailyWrite(obj, function(err,rows){
+        if(resultCode == dbConnection.OK){
+            res.json(rows);
+        } else {
+            console.log("false reason: id duplicated");
+            res.json({"success":false});
+        }
     });
 });
 
