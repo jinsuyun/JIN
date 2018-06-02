ba85c3bbd17ef04611757b9210bd19dde32f00de
jungwoo <okjwoo@ajou.ac.kr>
2018-06-03 03:59:43 +0900

    웹 로그인 그래프 서버

---
+++ public/javascripts/web_login.js
@@ -0,0 +1,36 @@
+$('#login_form').validate({
+  onkeyup: false,
+  submitHandler: function () {
+      console.log($('#id').val())
+    return true;
+  },
+  rules: {
+    id: {
+      required: true,
+      minlength: 1
+    },
+    password: {
+      required: true,
+      minlength: 1,
+      remote: {
+        url: '/weblogin',
+        type: 'post',
+        data: {
+          id: function () {
+              console.log($('#id').val())
+            return $('#id').val();
+          }
+        },
+        dataFilter: function (data) {
+          var data = JSON.parse(data);
+            console.log(data);
+          if (data.success) {
+            return true
+          } else {
+            return "\"" + data.msg + "\"";
+          }
+        }
+      }
+    }
+  }
+});
--- routes/web-main-router.js
+++ routes/web-main-router.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var webFoodAdapter = require('../adapters/webmain-db-adapter');
 
-/* GET home page. */
 router.get('/', function(req, res) {
+
+    var id=req.params.id;
     res.render('staty.html');
-    res.writeHead(200,{'Content-Type':'text/html'});
 });
 
 module.exports = router;
