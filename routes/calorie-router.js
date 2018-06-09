d7fa1e36ab55660423ddaae4e64d8db32c434e73
jungwoo <okjwoo@ajou.ac.kr>
2018-06-09 15:51:14 +0900

    칼로리 데이터 수정

--- routes/calorie-router.js
+++ routes/calorie-router.js
@@ -3,9 +3,12 @@ var router = express.Router();
 var calorieAdapter = require('../adapters/calorie-db-adapter');
 var dbConnection = require('./result');
 
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
 
-    calorieAdapter.calorieWrite(function(resultCode,rows){
+    var str = Object.keys(req.body);
+    var obj = JSON.parse(str[0]);
+
+    calorieAdapter.calorieWrite(obj, function(resultCode,rows){
         if(resultCode == dbConnection.OK){
             res.json({"success":true});
         } else {
