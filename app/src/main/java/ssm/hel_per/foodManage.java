f5b85a5747e90b40f9d00d3e6c351a0ea6efab20
jungwoo <okjwoo@ajou.ac.kr>
2018-06-10 05:22:52 +0900

    칼로리별 식단 추가

--- app/src/main/java/ssm/hel_per/foodManage.java
+++ app/src/main/java/ssm/hel_per/foodManage.java
@@ -14,8 +14,7 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-
-import static android.support.constraint.Constraints.TAG;
+import android.widget.TextView;
 
 public class foodManage  extends Fragment implements Main2Activity.OnBackPressedListener{
     home mainFragment;
@@ -23,6 +22,7 @@ public class foodManage  extends Fragment implements Main2Activity.OnBackPressed
     FrameLayout food;
     String bt;
     ImageView graph;
+    TextView tv;
     FloatingActionButton fab;
 
     public FloatingActionButton getFloatingActionButton() {
@@ -34,10 +34,23 @@ public class foodManage  extends Fragment implements Main2Activity.OnBackPressed
         v = inflater.inflate(R.layout.food_manage, container, false);
         v2 = inflater.inflate(R.layout.food_calori, container, false);
         food = (FrameLayout)v.findViewById(R.id.food);
-        graph =(ImageView)v2.findViewById(R.id.graph);
+        graph =(ImageView)v2.findViewById(R.id.foodinfograph);
+        tv = (TextView) v.findViewById(R.id.reqCalText);
         bt = getActivity().getIntent().getStringExtra("bodytype");
 
-        Log.d(TAG,"SSIBAL"+bt);
+        bodyAlgo bodyalgo = new bodyAlgo();
+
+        int age = getActivity().getIntent().getIntExtra("age", 0);
+        double height = getActivity().getIntent().getDoubleExtra("height", 0);
+        double weight = getActivity().getIntent().getDoubleExtra("weight", 0);
+        String sex = getActivity().getIntent().getStringExtra("sex");
+        double targetweight = getActivity().getIntent().getDoubleExtra("targetweight", 0);
+        int targetperiod = getActivity().getIntent().getIntExtra("targetperiod", 0);
+        int worklevel = getActivity().getIntent().getIntExtra("worklevel", 0);
+
+        int reqCalorie = (int) bodyalgo.eatCal(height, weight, age, sex, worklevel, targetweight, targetperiod);
+
+        tv.setText(String.valueOf(reqCalorie) + "kcal");
 
         mainFragment = new home();
         fab = (FloatingActionButton) v.findViewById(R.id.fab);
@@ -54,9 +67,9 @@ public class foodManage  extends Fragment implements Main2Activity.OnBackPressed
         });
 
         if(bt !=null) {
-            if (bt.equals("LW") || bt.equals("LF") || bt.equals("LB")) {
-                food.setBackgroundResource(R.drawable.menu_2100);
-                graph.setImageResource(R.drawable.nut_2100);
+            if (reqCalorie < 1150) {
+                food.setBackgroundResource(R.drawable.menu_1100);
+                graph.setImageResource(R.drawable.nut_1100);
                 food.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -67,7 +80,98 @@ public class foodManage  extends Fragment implements Main2Activity.OnBackPressed
                         dialog.show();
                     }
                 });
-            } else if (bt.equals("SB") || bt.equals("SW")) {
+            } else if (reqCalorie < 1250 && reqCalorie >= 1150) {
+                food.setBackgroundResource(R.drawable.menu_1200);
+                graph.setImageResource(R.drawable.nut_1200);
+                food.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialog dialog = new Dialog(getActivity());
+
+                        dialog.setContentView(R.layout.food_calori);
+                        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                        dialog.show();
+                    }
+                });
+            } else if (reqCalorie < 1350 && reqCalorie >= 1250) {
+                food.setBackgroundResource(R.drawable.menu_1300);
+                graph.setImageResource(R.drawable.nut_1300);
+                food.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialog dialog = new Dialog(getActivity());
+
+                        dialog.setContentView(R.layout.food_calori);
+                        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                        dialog.show();
+                    }
+                });
+            } else if (reqCalorie < 1450 && reqCalorie >= 1350) {
+                food.setBackgroundResource(R.drawable.menu_1400);
+                graph.setImageResource(R.drawable.nut_1400);
+                food.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialog dialog = new Dialog(getActivity());
+
+                        dialog.setContentView(R.layout.food_calori);
+                        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                        dialog.show();
+                    }
+                });
+            } else if (reqCalorie < 1550 && reqCalorie >= 1450) {
+                food.setBackgroundResource(R.drawable.menu_1500);
+                graph.setImageResource(R.drawable.nut_1500);
+                food.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialog dialog = new Dialog(getActivity());
+
+                        dialog.setContentView(R.layout.food_calori);
+                        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                        dialog.show();
+                    }
+                });
+            } else if (reqCalorie < 1650 && reqCalorie >= 1550) {
+                food.setBackgroundResource(R.drawable.menu_1600);
+                graph.setImageResource(R.drawable.nut_1600);
+                food.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialog dialog = new Dialog(getActivity());
+
+                        dialog.setContentView(R.layout.food_calori);
+                        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                        dialog.show();
+                    }
+                });
+            } else if (reqCalorie < 1750 && reqCalorie >= 1650) {
+                food.setBackgroundResource(R.drawable.menu_1700);
+                graph.setImageResource(R.drawable.nut_1700);
+                food.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialog dialog = new Dialog(getActivity());
+
+                        dialog.setContentView(R.layout.food_calori);
+                        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                        dialog.show();
+                    }
+                });
+            } else if (reqCalorie < 1850 && reqCalorie >= 1750) {
+                food.setBackgroundResource(R.drawable.menu_1800);
+                graph.setImageResource(R.drawable.nut_1800);
+                food.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialog dialog = new Dialog(getActivity());
+
+                        dialog.setContentView(R.layout.food_calori);
+                        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                        dialog.show();
+                    }
+                });
+            } else if (reqCalorie < 1950 && reqCalorie >= 1850) {
                 food.setBackgroundResource(R.drawable.menu_1900);
                 graph.setImageResource(R.drawable.nut_1900);
                 food.setOnClickListener(new View.OnClickListener() {
@@ -81,9 +185,8 @@ public class foodManage  extends Fragment implements Main2Activity.OnBackPressed
                     }
                 });
             } else {
-
-                food.setBackgroundResource(R.drawable.menu_1500);
-                graph.setImageResource(R.drawable.nut_1500);
+                food.setBackgroundResource(R.drawable.menu_2000);
+                graph.setImageResource(R.drawable.nut_2000);
                 food.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
