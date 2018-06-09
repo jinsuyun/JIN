c65b1b5ef9651ae29f6f89155d23595e93ed25e3
JINSUYUN <say2dbs@ajou.ac.kr>
2018-06-10 05:19:42 +0900

    운동 카운트 activity 생성

---
+++ app/src/main/java/ssm/hel_per/exercise_all/exercise_weak/weak_lv1/exercise_step.java
@@ -0,0 +1,31 @@
+package ssm.hel_per.exercise_all.exercise_weak.weak_lv1;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import ssm.hel_per.R;
+
+public class exercise_step extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_step);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
