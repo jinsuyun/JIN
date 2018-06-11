5f6021494e755cfcfd131c20562ae3d244bf0f89
jungwoo <okjwoo@ajou.ac.kr>
2018-06-11 21:59:41 +0900

    dailysearch rows수정

--- adapters/appdaily-db-adapter.js
+++ adapters/appdaily-db-adapter.js
@@ -29,7 +29,7 @@ adapter.dailySearch = function(id, cb) {
             cb(resultCode, []);
         } else { // db연결성공
             connection.query(dailySearchQuery, [id], function(err, rows) {
-                if (!rows[0]) { // daily x
+                if (rows == []) { // daily x
                     console.log(err);
                     resultCode = dbResult.Fail;
                     connection.release();
