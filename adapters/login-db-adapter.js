0fcce4861e28afaf30baa14d85ac5ef5d76f7582
jungwoo <okjwoo@ajou.ac.kr>
2018-06-11 20:56:48 +0900

    로그인 오류 수정

--- adapters/login-db-adapter.js
+++ adapters/login-db-adapter.js
@@ -16,7 +16,7 @@ var loginSearchQuery = 'SELECT * FROM appuser WHERE id=? AND password=?'; // id/
 var userDailySearchQuery = 'SELECT * FROM daily WHERE id=? AND workoutday=?';
 var createDailyQuery = 'INSERT INTO daily VALUE (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'; // daily 생성
 
-adapter.loginSearch = function(id, password, cb) {
+adapter.loginSearch = function(id, password, workoutday, cb) {
     var resultCode = dbResult.Fail;
 
     pool.getConnection(function(err, connection) {
@@ -33,12 +33,12 @@ adapter.loginSearch = function(id, password, cb) {
                     connection.release();
                     cb(resultCode, []);
                 } else { // 로그인 성공
-                    connection.query(userDailySearchQuery, [user.id, user.workoutday], function(err, rows) {
+                    connection.query(userDailySearchQuery, [id, workoutday], function(err, rows) {
                         if (!err) { // query가 오는 경우
                             console.log(rows);
                             if(!rows[0]) { // 중복 id x
                                 console.log('not duplicated id');
-                                connection.query(createDailyQuery, [user.id, user.workoutday, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], function(err) {
+                                connection.query(createDailyQuery, [id, workoutday, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], function(err) {
                                     if (err) {
                                         console.log(err)
                                         resultCode = dbResult.Fail;
