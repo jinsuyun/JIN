1a591078665c7894b1cadd4325b8f8ef5bf5a9bf
jungwoo <okjwoo@ajou.ac.kr>
2018-06-11 21:15:56 +0900

    rows 수정

--- adapters/login-db-adapter.js
+++ adapters/login-db-adapter.js
@@ -35,8 +35,8 @@ adapter.loginSearch = function(id, password, workoutday, cb) {
                 } else { // 로그인 성공
                     connection.query(userDailySearchQuery, [id, workoutday], function(err, rows2) {
                         if (!err) { // query가 오는 경우
-                            console.log(rows);
-                            if(!rows[0]) { // 중복 id x
+                            console.log(rows2);
+                            if(!rows2[0]) { // 중복 id x
                                 console.log('not duplicated id');
                                 connection.query(createDailyQuery, [id, workoutday, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], function(err) {
                                     if (err) {
