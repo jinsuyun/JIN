d7046cd8857d7a8b69848d768b2385f37ead0b15
jungwoo <okjwoo@ajou.ac.kr>
2018-06-11 21:35:31 +0900

    로그인 원상복귀

--- adapters/login-db-adapter.js
+++ adapters/login-db-adapter.js
@@ -13,10 +13,8 @@ var pool = mysql.createPool(dbConfig);
 var adapter = {};
 
 var loginSearchQuery = 'SELECT * FROM appuser WHERE id=? AND password=?'; // id/pw를 이용하여 유저 정보 search
-var userDailySearchQuery = 'SELECT * FROM daily WHERE id=? AND workoutday=?';
-var createDailyQuery = 'INSERT INTO daily VALUE (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'; // daily 생성
 
-adapter.loginSearch = function(id, password, workoutday, cb) {
+adapter.loginSearch = function(id, password, cb) {
     var resultCode = dbResult.Fail;
 
     pool.getConnection(function(err, connection) {
@@ -26,31 +24,16 @@ adapter.loginSearch = function(id, password, workoutday, cb) {
             connection.release();
             cb(resultCode, []);
         } else { // db연결성공
-            connection.query(loginSearchQuery, [id, password], function(err, rows1) {
+            connection.query(loginSearchQuery, [id, password], function(err, rows) {
                 if (err) { // 로그인 실패
                     console.log(err);
                     resultCode = dbResult.Fail;
                     connection.release();
                     cb(resultCode, []);
                 } else { // 로그인 성공
-                    connection.query(userDailySearchQuery, [id, workoutday], function(err, rows2) {
-                        if (!err) { // query가 오는 경우
-                            if(!rows2[0]) { // 중복 id x
-                                console.log('not duplicated id');
-                                connection.query(createDailyQuery, [id, workoutday, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], function(err) {
-                                    if (err) {
-                                        console.log(err)
-                                        resultCode = dbResult.Fail;
-                                    } else {
-                                        console.log("calorie success");
-                                        resultCode = dbResult.OK;
-                                    }
-                                    connection.release();
-                                    cb(resultCode, rows1);
-                                });
-                            } else {}
-                        } else {}
-                    });
+                    resultCode = dbResult.OK;
+                    connection.release();
+                    cb(resultCode, rows);
                 }
             });
         }
